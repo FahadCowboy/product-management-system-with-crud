fbabb7567d4f4cf360f12354d2dc772578bfed7d
FahadCowboy <olivierafahad@gmail.com>
2021-10-30 15:49:28 +0600

    project done

--- product-management-system-server/index.js
+++ product-management-system-server/index.js
@@ -1,1 +1,41 @@
+const { MongoClient } = require('mongodb')
 const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = process.env.PORT || 5000
+const ObjectId = require('mongodb').ObjectId
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+app.get('/user', (req, res) => {
+   console.log('Server is running well')
+   res.send('Server is running well')
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(port, () => {
+   console.log('This server is running at port no: ', port)
+})
